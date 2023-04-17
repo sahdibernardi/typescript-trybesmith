a5b36fc7dda026c27e57606a8154615e02a45906
Sahra Di Bernardi <sah.dibernardi@gmail.com>
2023-04-17 02:59:30 -0300

    add: requirement 06

--- src/routes/products.router.ts
+++ src/routes/products.router.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 import productsController from '../controllers/products.controller';
+import validateProducts from '../middlewares/validateProducts';
 
 const productsRouter = Router();
 
-productsRouter.post('/', productsController.newProduct);
+productsRouter.post(
+  '/',
+  validateProducts.nameValidation,
+  validateProducts.amountValidation,
+  productsController.newProduct,
+);
 productsRouter.get('/', productsController.listProducts);
 
 export default productsRouter;
